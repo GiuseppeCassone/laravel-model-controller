466aa44e2cb88f0e8ea71c13c875ddc8d5934912
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-04-09 14:52:15 +0200

    esercizio base finito

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,9 +6,18 @@
 
     <h1>MY FILM</h1>
 
+    @foreach($movies as $movie)
     <div class="card">
-        
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">TITOLO: {{ $movie->title }}</h5>
+            <p class="card-text">TITOLO ORIGINALE: {{ $movie->original_title}}</p>
+            <p class="card-text">NAZIONE: {{ $movie->nationality}}</p>
+            <p class="card-text">DATA DI USCITA: {{ $movie->date}}</p>
+            <p class="card-text">VOTO: {{ $movie->vote}}</p>
+        </div>
     </div>
+    @endforeach
 </div>
 
 
