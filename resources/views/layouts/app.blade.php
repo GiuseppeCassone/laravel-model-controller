fd426f7893e52f7c58d9f5e64b8c4cdf54380e04
GiuseppeCassone <151375774+GiuseppeCassone@users.noreply.github.com>
2024-04-09 13:53:51 +0200

    Initial commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    @vite('resources/js/app.js')
+</head>
+<body>
+
+    <div class="container">
+        <h1>ciao</h1>
+    </div>
+    
+</body>
+</html>
