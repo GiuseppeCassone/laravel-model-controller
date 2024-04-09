4f3f8492ec14e6a6b7567ae5e489a0b55bb1c1d5
giuseppe_cassone <giuseppecassone7@gmail.com>
2024-04-09 15:47:54 +0200

    inserito immagine di background, cambiato colori al testo, dimensioni delle card e la loro visualizzazione in pagina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,27 @@
 
 @section('content')
 
-<div class="container">
+<div class="container mt-5 p-5">
 
-    <h1>MY FILM</h1>
+    <h1 class="text-center mb-5">MY FILM</h1>
 
-    @foreach($movies as $movie)
-    <div class="card">
-        <img src="..." class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">TITOLO: {{ $movie->title }}</h5>
-            <p class="card-text">TITOLO ORIGINALE: {{ $movie->original_title}}</p>
-            <p class="card-text">NAZIONE: {{ $movie->nationality}}</p>
-            <p class="card-text">DATA DI USCITA: {{ $movie->date}}</p>
-            <p class="card-text">VOTO: {{ $movie->vote}}</p>
+    <div class="row flex-wrap">
+
+        @foreach($movies as $movie)
+        <div class="card align-items-center">
+            <img src="..." class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">TITOLO: {{ $movie->title }}</h5>
+                <p class="card-text"><strong>TITOLO ORIGINALE: </strong>{{ $movie->original_title}}</p>
+                <p class="card-text"><strong>NAZIONE: </strong>{{ $movie->nationality}}</p>
+                <p class="card-text"><strong>DATA DI USCITA: </strong>{{ $movie->date}}</p>
+                <p class="card-text"><strong>VOTO: </strong>{{ $movie->vote}}</p>
+            </div>
         </div>
+        @endforeach
+
     </div>
-    @endforeach
+
 </div>
 
 
